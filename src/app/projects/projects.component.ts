8252d44e992dc4d5b07a25b882446c3289577a1d
Trev-D-Dev <dunn.trevor@pm.me>
2025-04-11 13:49:46 -0400

    Working on styling projects page

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -22,4 +22,34 @@ export class ProjectsComponent {
     const data = await response.json();
     this.projects = data.projects;
   }
+
+  /**
+   * 
+   * @param event the event that triggered the function call
+   */
+  showDetails(event: any): void {
+
+    const clickedProject = event.currentTarget;
+    const details = clickedProject.querySelector(".project-details");
+
+    //Determines if project is visible and sets attributes accordingly
+    if(!details.hidden) {
+      details.hidden = true;
+      clickedProject.setAttribute('class', 'project');
+      return;
+    }
+
+    //Checks if any projects are already selected, if one is it is unselected
+    if(document.querySelectorAll('.project.selected').length != 0) {
+      const selectedProject = document.querySelector('.project.selected');
+      selectedProject?.setAttribute('class', 'project');
+
+      const details = selectedProject?.querySelector('.project-details');
+      details?.setAttribute('hidden', 'true');
+    }
+
+    //Changes hidden and class attributes
+    details.hidden = false;
+    clickedProject.setAttribute('class', 'project selected');
+  }
 }
